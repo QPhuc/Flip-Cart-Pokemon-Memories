f951732aa6eed8377b2a7ad1977f50d6c9c36bae
Qphuc <tqp1610@gmail.com>
2021-08-03 22:55:04 +0700

    css card

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <h1>Heelo</h1>
+    <MainScreen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)"/>
+    <InteractScreen v-if="statusMatch === 'match'"/>
   </div>
 </template>
 
 <script>
-
+import MainScreen from './components/MainScreen.vue'
+import InteractScreen from './components/InteractScreen.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      statusMatch: 'default'
+    }
+  },
   components: {
-
+    MainScreen,
+    InteractScreen
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running handle", config);
+      this.statusMatch = "match"
+    }
   }
 }
 </script>
--- src/components/InteractScreen.vue
+++ src/components/InteractScreen.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-
+    <div class="screen">
+        <p>Interact is here ...</p>
+        <CardFlip />
     </div>
 </template>
 
 <script>
+    import CardFlip from './Card.vue'
     export default {
         name: 'InteractScreen',
+        components: {
+            CardFlip
+        }
     }
 </script>
 
